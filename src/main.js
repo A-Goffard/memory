0eb55e5345d00eb8f533ea059418e0dfb708aa6e
A-Goffard <aintzagoffard@gmail.com>
2024-03-15 09:34:36 +0100

    fitxas

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import Vue from 'vue';
-import VueParticles from 'vue-particles';
+
 
 createApp(App).use(router).mount('#app');
-/* Vue.use(VueParticles); */
+
--- src/views/Equipo.vue
+++ src/views/Equipo.vue
@@ -1,11 +1,57 @@
 <template>
+    <div>
+        <h1>Equipo</h1>
+        <div class="contenedor-cards">
+            <div v-for="fitxa in fitxas" :key="fitxa.id">
+                <FitxasEquipo :fitxa="fitxa" />
+            </div>
+        </div>
+    </div>
     
 </template>
 
-<script setup>
+<script>
+import FitxasEquipo from '../components/FitxasEquipo.vue';
 
+export default {
+    components: {
+        FitxasEquipo
+    },
+    data() {
+        return {
+            fitxas: [
+                {
+                    id: 1,
+                    nombre: 'Jesús Romero',
+                    puesto: 'Srum Master',
+                    trabajo: 'Developer',
+                    imagen: '/public/img/jesus.jpeg',
+                },
+                {
+                    id: 2,
+                    nombre: 'Jonathan Dominguez',
+                    puesto: 'Coordinator',
+                    trabajo: 'Developer',
+                    imagen: '/public/img/Jonny.jpeg',
+                },
+                {
+                    id: 3,
+                    nombre: 'Aintzane Goffard',
+                    puesto: 'Product Owner',
+                    trabajo: 'Developer',
+                    imagen: '/public/img/Aintzane.jpeg',
+                },
+            ]
+        }
+    }
+}
 </script>
 
 <style scoped>
-
+.contenedor-general {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+  }
 </style>
