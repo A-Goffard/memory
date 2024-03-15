d2b2e738e96698b148abe9ac05736411d32e13ce
JonathanADB <jhonnysoy1@gmail.com>
2024-03-15 09:40:41 +0100

    agrego nuevo juego

--- src/views/Juegos.vue
+++ src/views/Juegos.vue
@@ -2,8 +2,8 @@
     <div class="divBotones">
         <button @click="goTo('/memorygame')"> Memory Game </button>
         <button @click="goTo('/tictactoe')">Tic Tac Toe</button>
-        <button>Juego</button>
-        <button>Juego</button>
+        <button></button>
+        <button>jonathantetris</button>
         <button>Juego</button>
         <button>Juego</button>
         <button>Juego</button>
